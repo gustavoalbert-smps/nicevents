6a7f66c70fd7394d94c135ce02f29cd368a2c9cb
Gustavo Albert <gustavoads.millionary@gmail.com>
2023-02-01 20:08:58 -0300

    feature: detail event page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
 					<p class="card-date">25/01/2023</p>
 					<h5 class="card-title">{{$event->title}}</h5>
 					<p class="card-participants">X participantes</p>
-					<a href="#" class="btn btn-primary">Saber mais</a>
+					<a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
 				</div>
 			</div>
 			@endforeach
