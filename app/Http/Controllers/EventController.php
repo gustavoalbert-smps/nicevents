4d12d34a05bf910b4ac919cc6e04ede712e77015
Gustavo Albert <gustavoads.millionary@gmail.com>
2023-02-07 12:01:48 -0300

    feature: event owner in event detail page

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 Use App\Models\Event;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
@@ -32,7 +33,9 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
     
     public function store(Request $request) {
@@ -59,6 +62,9 @@ class EventController extends Controller
 
             $event->image = $imageName;
         }
+
+        $user = auth()->user();
+        $event->user_id = $user->id;
 
         $event->save();
 
