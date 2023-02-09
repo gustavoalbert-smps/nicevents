635bb93052de517bf8f9b9bbc3588aa916ae807d
Gustavo Albert <gustavoads.millionary@gmail.com>
2023-02-09 19:33:03 -0300

    feature: showing events with confirmed presence for the user

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -46,4 +46,35 @@
         <p>Você ainda não tem eventos, <a href="/events/create">Crie um evento</a></p>
     @endif
 </div>
+<div class="col-md-10 offset-md-1 dashboard-title-container">
+    <h1>Meus eventos marcados</h1>
+</div>
+<div class="col-md-10 offset-md-1 dashboard-events-container">
+    @if(count($eventsasparticipant) > 0)
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Participantes</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($eventsasparticipant as $event)
+                    <tr>
+                        <td scope="row">{{ $loop->index+1 }}</td>
+                        <td><a href="/events/{{$event->id}}">{{$event->title}}</a></td>
+                        <td>{{ count($event->users)}}</td>
+                        <td>
+                            <a href="#">Sair</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>Você ainda não marcou presença em nenhum evento.</p>
+    @endif
+</div>
 @endsection
